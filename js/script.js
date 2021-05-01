1d71bca9615f8c7cf2bdbcae7547cbf360cf9fde
Ronak Shah <37ronakshah@gmail.com>
2021-05-01 18:53:35 -0400

    Added Javascript code for loading categories.

--- js/script.js
+++ js/script.js
@@ -19,3 +19,48 @@ const setThumb = function (event) {
 
 // Listen to the entire gallery for a click
 theGallery.addEventListener(`click`, setThumb)
+
+
+// Data for Grocery Categories.
+let categories = [
+  {
+    categoryLabel: 'Vegetables and Fruits',
+    altDetailsImage: 'Vegetables and Fruits Category',
+    categoryImagePath: 'img/categories/vegetables-and-fruits-farmers-market.jpg'
+  },
+  {
+    categoryLabel: 'Bakery and Dairy',
+    altDetailsImage: 'Bakery and Dairy Category',
+    categoryImagePath: 'img/categories/bakerycakesdiary-250x250-1.png'
+  },
+  {
+    categoryLabel: 'Beauty and Hygiene',
+    altDetailsImage: 'Beauty and Hygiene Category',
+    categoryImagePath: 'img/categories/Beauty_and_Hygiene-270x270.jpg'
+  },
+  {
+    categoryLabel: 'Cleaning Supplies',
+    altDetailsImage: 'Cleaning Supplies Category',
+    categoryImagePath: 'img/categories/cleaning-supplies-kit.jpg'
+  },
+  {
+    categoryLabel: 'Click here for all categories...',
+    altDetailsImage: 'All Categories',
+    categoryImagePath: 'img/categories/arrow.jpg'
+  }
+];
+
+//Code to load data of grocery categories.
+let categoriesDiv = document.querySelector('.categories');
+
+categories.forEach((category) => {
+    let newCategoryElement = document.createElement(`div`);
+    newCategoryElement.classList.add(`category`);
+    newCategoryElement.innerHTML = `
+    <img src="${category.categoryImagePath}" alt="${category.altDetailsImage}">
+          <br>
+          <span>${category.categoryLabel}</span>
+          <!-- Ratings  -->
+    `
+    categoriesDiv.appendChild(newCategoryElement);
+});
